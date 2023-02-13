c9fdd55f9ab72a6d05eeeccfa1554b41837b900e
tyler <tyler.wood@nakedwines.com>
2023-02-12 18:37:57 -0700

    adding firehose to s3 bucket

--- iam.tf
+++ iam.tf
@@ -50,7 +50,7 @@ EOF
 resource "aws_iam_policy" "allow_kinesis_processing" {
   name        = "allow_kinesis_processing"
   path        = "/"
-  description = "IAM policy for logging from a lambda"
+  description = ""
 
   policy = <<EOF
 {
@@ -90,4 +90,61 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
 resource "aws_iam_role_policy_attachment" "kinesis_processing" {
   role       = aws_iam_role.iam_for_lambda.name
   policy_arn = aws_iam_policy.allow_kinesis_processing.arn
+}
+
+resource "aws_iam_role" "firehose_role" {
+  name = "firehose_test_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement":
+  [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "firehose.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  inline_policy {
+    name = "kinesis-s3-inline-policy"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {      
+          Effect = "Allow",      
+          Action = [
+            "s3:AbortMultipartUpload",
+            "s3:GetBucketLocation",
+            "s3:GetObject",
+            "s3:ListBucket",
+            "s3:ListBucketMultipartUploads",
+            "s3:PutObject"
+          ]      
+          Resource = [        
+            "arn:aws:s3:::tjw-kinesis-demo-test-bucket",
+            "arn:aws:s3:::tjw-kinesis-demo-test-bucket/*"            
+          ]    
+        },
+        {
+          Effect = "Allow"
+          Action = [
+            "kinesis:DescribeStream",
+            "kinesis:GetShardIterator",
+            "kinesis:GetRecords",
+            "kinesis:ListShards"
+          ]
+          Resource = [
+            "arn:aws:kinesis:*" 
+          ]
+        }
+      ]
+    })
+  }
 }
